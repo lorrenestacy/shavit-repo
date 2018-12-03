bb44bc3816b73c4a6d569f26cd4d717c85ac06cf
Ayganv <virg.baroni@gmail.com>
2018-12-03 01:11:02 -0200

    redesign of bottom navigation to use fragments as the content of the activity

--- Shavit/app/src/main/java/com/example/lorreneogbonna/shavit/views/activities/CadastroActivity.java
+++ Shavit/app/src/main/java/com/example/lorreneogbonna/shavit/views/activities/CadastroActivity.java
@@ -1,10 +1,12 @@
-package com.example.lorreneogbonna.shavit;
+package com.example.lorreneogbonna.shavit.views.activities;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.lorreneogbonna.shavit.R;
 
 public class CadastroActivity extends AppCompatActivity {
 
