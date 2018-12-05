64b1ec196cf73ad26e7fc03175ba9ee21f8b4beb
Lorrene Ogbonna <lorreneogbonna@Lorrenes-MBP.lan>
2018-12-05 18:02:53 +0000

    logout button

--- Shavit/app/src/main/java/com/example/lorreneogbonna/shavit/views/fragments/PerfilClinicaFragment.java
+++ Shavit/app/src/main/java/com/example/lorreneogbonna/shavit/views/fragments/PerfilClinicaFragment.java
@@ -1,12 +1,16 @@
 package com.example.lorreneogbonna.shavit.views.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.lorreneogbonna.shavit.R;
+import com.example.lorreneogbonna.shavit.views.activities.LoginActivity;
 
 public class PerfilClinicaFragment extends Fragment {
 
@@ -16,11 +20,23 @@ public class PerfilClinicaFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container,
+                             final ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.fragment_perfil_clinica, container, false);
+        final Button buttonLogout = view.findViewById(R.id.logoutClinica);
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            Context thiscontext = container.getContext();
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thiscontext, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil_clinica, container, false);
+        return view;
     }
 
 }
