bca8d9a639c070a818636eda5e005f752b22dfd3
Ayganv <virg.baroni@gmail.com>
2019-01-03 23:55:06 -0200

    layout of activity 'agendamento'

--- Shavit/app/src/main/java/com/example/lorreneogbonna/shavit/views/activities/AgendamentoActivity.java
+++ Shavit/app/src/main/java/com/example/lorreneogbonna/shavit/views/activities/AgendamentoActivity.java
@@ -12,50 +12,11 @@ import com.example.lorreneogbonna.shavit.R;
 
 public class AgendamentoActivity extends AppCompatActivity {
 
-    public static final String EXTRA_SERVICO_NAME_KEY = "nameServicoId";
-    public static final String EXTRA_SERVICO_DESCRIPTION_KEY = "descriptionServicoId";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_servico);
-
-        //getting components
-        EditText txtServicoNameText = findViewById(R.id.txtNomeServico);
-        EditText txtServicoDescriptionText = findViewById(R.id.txtDescricaoServico);
-        Button btnConfirm = findViewById(R.id.btnAddServicoEdit);
-        Button btnCancel = findViewById(R.id.btnCancelServicoEdit);
-
-        //getting possible values for intent
-        Intent intent = getIntent();
-        String nameService = intent.getStringExtra(AgendamentoActivity.EXTRA_SERVICO_NAME_KEY);
-        String descriptionService = intent.getStringExtra(AgendamentoActivity.EXTRA_SERVICO_DESCRIPTION_KEY);
-
-        txtServicoNameText.setText(nameService == null ? "" : nameService);
-        txtServicoDescriptionText.setText(descriptionService == null ? "" : descriptionService);
-
-        btnConfirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //placeholder operation
-                Toast.makeText(AgendamentoActivity.this,
-                        "Operação realizada com sucesso",
-                        Toast.LENGTH_SHORT).show();
-
-                //just going back to parent activity
-                finish();
-            }
-        });
-
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
+        setContentView(R.layout.activity_agendamento);
 
     }
 
